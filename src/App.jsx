7a62e826b3997077f62f83f40dc0b1be8e37a06c
Abhay yadav <yadavabhay8227.com>
2025-01-15 22:55:26 +0530

    ext fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Navbar from "./component/Navbar";
 import Background from "./component/Page/BackGround";
 import QrGenerator from "./component/Page/QrGeneratoer";
 import BackgroundRemover from "./component/Page/BackgroundRemover";
-
 import Pdf_doc from "./component/Page/Pdf_doc";
 import ImageEnhancer from "./component/Page/ImageEnhancer";
 import Resizer from "./component/Page/Resizer";
-import Doc_pdf from "./component/Page/Doc_pdf";
-
+import Doc_pdf from "./component/Page/Doc_pdf.jsx";
 
 const App = () => {
   return (
@@ -27,19 +25,10 @@ const App = () => {
           <Route path="/qr" element={<QrGenerator />} />
           <Route path="/bg-remove" element={<BackgroundRemover />} />
           {/* <Route path="/file-convertor" element={<FileConverter />} /> */}
-          <Route path="/doc-pdf" element={<Doc_pdf/>} />
-          <Route path="/pdf-doc" element={<Pdf_doc/>} />
-          <Route path="/image-enhancer" element={<ImageEnhancer/>} />
-          <Route path="/image-resize" element={<Resizer/>} />
-
-
-
-
-
-
-
-
-
+          <Route path="/doc-pdf" element={<Doc_pdf />} />
+          <Route path="/pdf-doc" element={<Pdf_doc />} />
+          <Route path="/image-enhancer" element={<ImageEnhancer />} />
+          <Route path="/image-resize" element={<Resizer />} />
         </Routes>
       </div>
     </div>
